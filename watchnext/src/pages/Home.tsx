e4332321093b6d78094cf7743995c2f3095b968b
fatma <fatmakaddour26@gmail.com>
2025-09-23 14:11:58 +0200

    fixed header/footer

--- watchnext/src/pages/Home.tsx
+++ watchnext/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import {
-    IonContent,
     IonFooter,
     IonHeader,
     IonPage,
@@ -9,42 +8,40 @@ import {
     IonIcon,
     IonButton
 } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import {helpCircle, home, personCircle, search} from "ionicons/icons";
 
 const Home: React.FC = () => {
-  // @ts-ignore
     return (
-    <IonPage>
-      <IonHeader>
-          <IonToolbar>
-              <IonTitle>Search a content </IonTitle>
-          </IonToolbar>
-          <IonToolbar>
-              <IonSearchbar color="dark"
-              ></IonSearchbar>
-          </IonToolbar>
-      </IonHeader>
-        <IonFooter>
-            <IonToolbar>
-                <div className="ion-align-content-around">
-                    <IonButton >
-                        <IonIcon slot="icon-only" icon={home}></IonIcon>
-                    </IonButton>
-                <IonButton >
-                    <IonIcon slot="icon-only" icon={search}></IonIcon>
-                </IonButton>
-
-                <IonButton>
-                    <IonIcon  slot="icon-only" icon={helpCircle}></IonIcon>
-                </IonButton>
-                </div>
-            </IonToolbar>
-        </IonFooter>
-    </IonPage>
-  );
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Search a content </IonTitle>
+                </IonToolbar>
+                <IonToolbar>
+                    <IonSearchbar color="dark"
+                    ></IonSearchbar>
+                </IonToolbar>
+            </IonHeader>
+            <IonFooter>
+                <IonToolbar>
+                    <div className="ion-align-content-around">
+                        <IonButton>
+                            <IonIcon slot="icon-only" icon={home}></IonIcon>
+                        </IonButton>
+                        <IonButton>
+                            <IonIcon slot="icon-only" icon={search}></IonIcon>
+                        </IonButton>
+                        <IonButton>
+                            <IonIcon slot="icon-only" icon={helpCircle}></IonIcon>
+                        </IonButton>
+                    </div>
+                </IonToolbar>
+            </IonFooter>
+        </IonPage>
+    );
 };
+
 
 
 export default Home;
