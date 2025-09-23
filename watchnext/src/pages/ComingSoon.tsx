9930a17e9b67acef86f3520e03a694a6bcf5527b
Zaher ABBAS <z.abbas83@yahoo.com>
2025-09-23 16:28:26 +0200

    Added movie genres display to "Coming Soon" page and enhanced movie fetching logic

--- watchnext/src/pages/ComingSoon.tsx
+++ watchnext/src/pages/ComingSoon.tsx
@@ -1,4 +1,5 @@
 import {
+    IonBadge,
     IonButton,
     IonButtons,
     IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
@@ -14,7 +15,14 @@ import {
 } from '@ionic/react';
 import './Home.css';
 import React, {useEffect, useState} from "react";
-import {Movie, getUpcomingMovies, MoviesResponse, TMDB_IMG_BASE} from "../data/MoviesData";
+import {
+    Movie,
+    getUpcomingMovies,
+    MoviesResponse,
+    TMDB_IMG_BASE,
+    Genre,
+    getMoviesGenresList
+} from "../data/MoviesData";
 import {chevronBack, chevronForward} from "ionicons/icons";
 
 const ComingSoon: React.FC = () => {
@@ -26,24 +34,49 @@ const ComingSoon: React.FC = () => {
     const [imgBaseURL, setImgBaseURL] = useState(TMDB_IMG_BASE);
     const [totalPages, setTotalPages] = useState<number>(0);
     const [totalResults, setTotalResults] = useState<number>(0);
-
+    const [genres, setGenres] = useState<Genre[]>([]);
 
     useEffect(() => {
-            getUpcomingMovies(page).then(res => {
-                setMovieResponse(res)
-                setMovies(res!.results);
-                setLoading(false);
-                setTotalPages(res!.total_pages);
-                setTotalResults(res!.total_results);
-            }).catch(err => {
-                setLoading(false);
-                setError(err)
-            });
+        getUpcomingMovies(page).then(res => {
+            setMovieResponse(res)
+            setMovies(res!.results);
+            setTotalPages(res!.total_pages);
+            setTotalResults(res!.total_results);
+        }).then(() => {
+                getGenres();
+            }
+        ).catch(err => {
+            setLoading(false);
+            setError(err)
+        });
     }, [page]);
 
     useEffect(() => {
-        console.log("page:" , page)
-    }, [page]);
+        movies.forEach(movie => {
+            getMoviesGenresNames(movie);
+        })
+        setLoading(false);
+    }, [movies, genres]);
+
+    function getGenres() {
+        getMoviesGenresList().then(res => {
+            setGenres(res!);
+            console.log("genres:", res);
+        }).catch(err => {
+            setError(err);
+        });
+    }
+
+    function getMoviesGenresNames(movie: Movie) {
+        movie.genre_names = [];
+        movie.genre_ids.forEach(id => {
+            const genre = genres.find(g => g.id === id);
+            if (genre) {
+                movie.genre_names.push(genre.name);
+            }
+        })
+        console.log("movie:", movie);
+    }
 
     return (
         <IonPage>
@@ -64,46 +97,56 @@ const ComingSoon: React.FC = () => {
                         </IonButtons>
                     </IonToolbar>
                 </IonHeader>
-                <IonItem>
+                    <IonItem>
                     <IonLabel className="ion-text-center">{totalResults} movies found</IonLabel>
                 </IonItem>
-                <IonItem lines="none">
+                    <IonItem lines="none">
                     <IonGrid style={{ width: '100%' }}>
-                        <IonRow className="ion-align-items-center ion-justify-content-center">
-                            <IonCol size="auto">
-                                <IonButton onClick={() => setPage(p => p-1)} disabled={page <= 1}>
-                                    <IonIcon slot="icon-only" icon={chevronBack} />
-                                </IonButton>
-                            </IonCol>
-                            <IonCol size="auto">
-                                <IonLabel className="ion-text-center">Page {page} / {totalPages}</IonLabel>
-                            </IonCol>
-                            <IonCol size="auto">
-                                <IonButton onClick={() => setPage(p => p+1)} disabled={page >= totalPages}>
-                                    <IonIcon slot="icon-only" icon={chevronForward} />
-                                </IonButton>
-                            </IonCol>
-                        </IonRow>
-                    </IonGrid>
-                </IonItem>
-                <IonGrid>
-                    <IonRow>
-                        {movies.map(movie => (
-                            <IonCol size="6" size-sm="12" size-md="6" size-lg="4" size-xl="2" key={movie.id}>
-                                <IonCard>
-                                    <IonImg src={`${imgBaseURL}/w154/${movie.poster_path}`} alt={movie.title} />
-                                    <IonCardHeader>
-                                        <IonCardTitle>{movie.title}</IonCardTitle>
-                                        <IonCardSubtitle>
-                                            Sortie Le: {movie.release_date ? new Date(movie.release_date).toLocaleDateString() : "N/A"}
-                                        </IonCardSubtitle>
-                                    </IonCardHeader>
-                                    <IonCardContent />
-                                </IonCard>
-                            </IonCol>
-                        ))}
-                    </IonRow>
-                </IonGrid>
+                <IonRow className="ion-align-items-center ion-justify-content-center">
+                    <IonCol size="auto">
+                        <IonButton onClick={() => setPage(p => p-1)} disabled={page <= 1}>
+                            <IonIcon slot="icon-only" icon={chevronBack} />
+                        </IonButton>
+                    </IonCol>
+                    <IonCol size="auto">
+                        <IonLabel className="ion-text-center">Page {page} / {totalPages}</IonLabel>
+                    </IonCol>
+                    <IonCol size="auto">
+                        <IonButton onClick={() => setPage(p => p+1)} disabled={page >= totalPages}>
+                            <IonIcon slot="icon-only" icon={chevronForward} />
+                        </IonButton>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+        </IonItem>
+    <IonGrid>
+        {!loading &&
+        <IonRow>
+            {movies.map(movie => (
+                <IonCol size="6" size-sm="12" size-md="6" size-lg="4" size-xl="2" key={movie.id}>
+                    <IonCard>
+                        <IonImg src={`${imgBaseURL}/w154/${movie.poster_path}`} alt={movie.title} />
+                        <IonCardHeader>
+                            <IonCardTitle>{movie.title}</IonCardTitle>
+                            <IonGrid>
+                                <IonRow>
+                                    {movie.genre_names && movie.genre_names.map((name, index) => (
+                                        <IonCol key={index} size="auto">
+                                            <IonBadge color="tertiary">{name}</IonBadge>
+                                        </IonCol>
+                                    ))}
+                                </IonRow>
+                            </IonGrid>
+                            <IonCardSubtitle>
+                                Sortie Le: {movie.release_date ? new Date(movie.release_date).toLocaleDateString() : "N/A"}
+                            </IonCardSubtitle>
+                        </IonCardHeader>
+                        <IonCardContent />
+                    </IonCard>
+                </IonCol>
+            ))}
+        </IonRow>}
+    </IonGrid>
             </IonContent>
         </IonPage>
     );
