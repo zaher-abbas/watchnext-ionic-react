ed0554311f35e5d337dd34bbbdf3166dd1cdb4ea
Mouhammad ABBAS <z.abbas83@yahoo.com>
2025-09-24 14:48:01 +0200

    Update MoviesData.tsx

--- watchnext/src/data/MoviesData.tsx
+++ watchnext/src/data/MoviesData.tsx
@@ -51,6 +51,7 @@ const LANGUAGE = 'fr-FR';
 const API_TEKON= 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTg1NDEzZDMyODE2OWRkMWIzZDdhODlkNmRjNjZhMSIsIm5iZiI6MTc1ODU1MTMwMi44NDMwMDAyLCJzdWIiOiI2OGQxNWQwNjU1MmQ2NmJjM2U3YTRiNGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.-rHmKaU3Pe92r_aZPD6yCAmA0DZExax8zD8VLoTQAhM';
 const TMDB_BASE = "https://api.themoviedb.org/3";
 export const TMDB_IMG_BASE = "https://image.tmdb.org/t/p";
+
 export const getUpcomingMovies = async (page: number): Promise<MoviesResponse | null> => {
     const url = `${TMDB_BASE}/movie/upcoming?language=${LANGUAGE}&page=${page}`;
     const options = {
@@ -67,6 +68,26 @@ export const getUpcomingMovies = async (page: number): Promise<MoviesResponse | 
     } catch (error) {
         console.error('Error fetching upcoming movies:', error);
         return null;
+    }
+};
+
+export const getMovie = async (movie_id:number): Promise<Movie |null> => {
+    const url = `${TMDB_BASE}/movie/${movie_id}?language=${LANGUAGE}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_TEKON}`
+        }
+    }
+    try {
+        const res = await fetch(url, options)
+        return await res.json();
+    }
+    catch (error) {
+        console.error('Error fetching movie details:', error);
+        return null;
+
     }
 };
 
@@ -132,14 +153,5 @@ export const getMovieVideo = async (movie_id:number): Promise<VideosResponse | n
         return null;
     }
 }
-
-
-        const data = await res.json();
-        return data.genres;
-    }
-    catch (error) {
-        console.error('Error fetching movies genres:', error);
-        return null;
-    }
-}
+    
 
