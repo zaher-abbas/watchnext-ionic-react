c6ef0c74587040c1c1592f17d197e66f441769d3
shalom-gnassounou <shalom.gnassounou@labom2iformation.fr>
2025-09-25 10:34:33 +0200

    Test des routes

--- watchnext/src/App.test.tsx
+++ watchnext/src/App.test.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import{ MemoryRouter} from "react-router";
+import ComingSoon from "./pages/ComingSoon";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import * as path from "node:path";
 
-test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
-  expect(baseElement).toBeDefined();
+const routes = [
+    {path: '/', exact: true, component: Home},
+    {path: '/coming-soon', component: ComingSoon},
+    {path: '/movie/:2', component: Detail},
+]
+
+test.each(routes)("navigates to $path and render correct page",({path}) => {
+ render(
+     <MemoryRouter initialEntries={[path]}>
+         <App />
+     </MemoryRouter>
+ )
 });
